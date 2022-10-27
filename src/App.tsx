3b98756e7dde668182677631a503390716b3bca7
Eduardo Nascimento <jebnascimento@outlook.com>
2022-10-26 23:35:27 -0300

    create transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,27 @@
 import { Dashboard } from "./components/Dashboard";
-import Modal from 'react-modal'
+import Modal from "react-modal";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { TransactionsProvider } from "./TransactionsContext";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState<boolean>(false);
 
-  const [
-    isNewTransactionModalOpen,
-    setIsNewTransactionModalOpen ] = useState<boolean>(false);
-
-  function handleOpenNewTransactionModal () {
+  function handleOpenNewTransactionModal() {
     setIsNewTransactionModalOpen(true);
   }
 
-  function handleCloseNewTransactionModal () {
+  function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false);
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
@@ -30,6 +29,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,15 +1,18 @@
-import incomeImg from '../../assets/income.svg'
-import outcomeImg from '../../assets/outcome.svg'
-import totalImg from '../../assets/total.svg'
+import { useContext } from "react";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
 export function Summary() {
+  const transactions = useContext(TransactionsContext);
   return (
     <Container>
       <div>
         <header>
           <p>Entradas</p>
-          <img src={incomeImg} alt="entradas"/>
+          <img src={incomeImg} alt="entradas" />
         </header>
         <strong>R$1000,00</strong>
       </div>
@@ -17,18 +20,18 @@ export function Summary() {
       <div>
         <header>
           <p>Saídas</p>
-          <img src={outcomeImg} alt="saídas"/>
+          <img src={outcomeImg} alt="saídas" />
         </header>
         <strong>-R$500,00</strong>
       </div>
 
-      <div className='highlight-background'>
+      <div className="highlight-background">
         <header>
           <p>Total</p>
-          <img src={totalImg} alt="total"/>
+          <img src={totalImg} alt="total" />
         </header>
         <strong>R$500,00</strong>
       </div>
     </Container>
-  )
+  );
 }
